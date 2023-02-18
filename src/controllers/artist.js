dbca3019892e96aaf758a6d019e4e050385545cd
Sarah Moran <s.l.a.moran@gmail.com>
2023-02-18 13:17:32 +0000

    Update of code for SQL injection prevention

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -4,22 +4,22 @@ const createArtist = async (req, res) => {
   const { name, genre } = req.body
 
   try {
-    const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+    const { rows: [ artist ] } = await db.query('INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *', [name, genre])
     res.status(201).json(artist)
   } catch (err) {
     res.status(500).json(err.message)
   }
 }
 
-/*const createArtist = async (req, res) => {
-  const { name, genre } = req.body
+/*const createArtist =  async (req, res) => {
+    const { name, genre } = req.body
 
-  try {
-    const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
-    res.status(201).json(artist)
-  } catch (err) {
-    res.status(500).json(err.message)
-  }
+    try {
+        const { rows: [ artist ] } =  await db.query(`INSERT INTO Artists (name, genre ) VALUES ('${name}', ${genre}') RETURNING *`)
+        res.status(201).json(artist)
+    }   catch (err) {
+        res.status(500).json(err.message)
+    }
 }*/
 
 
