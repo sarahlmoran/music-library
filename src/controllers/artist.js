37461c0baa60086c4d35c5d29500e57fccaa652b
Sarah Moran <s.l.a.moran@gmail.com>
2023-02-18 11:54:58 +0000

    completion of creating artist, creation and migration of database

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,6 +1,26 @@
+const db = require('../db/index');
 
-const createArtist = (req,res) => {
-    res.sendStatus(201);
+const createArtist = async (req, res) => {
+  const { name, genre } = req.body
+
+  try {
+    const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+    res.status(201).json(artist)
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
 }
 
-module.exports = {createArtist}
+/*const createArtist = async (req, res) => {
+  const { name, genre } = req.body
+
+  try {
+    const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+    res.status(201).json(artist)
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
+}*/
+
+
+module.exports = { createArtist }
