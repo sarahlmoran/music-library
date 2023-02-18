ca2b8c6f5104795ec8db5bbb0d70854626888e0d
Sarah Moran <s.l.a.moran@gmail.com>
2023-02-18 16:32:38 +0000

    completion of reading single artists test and function

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -21,4 +21,23 @@ const readArtist = async (_, res) => {
     }
 }
 
-module.exports = { createArtist, readArtist }
+const getArtistByID = async (req,res) => {
+    const { id } = req.params
+
+    try {
+        const { rows: [artist] } = await db.query('SELECT * FROM Artists WHERE id = ($1)', [id])
+
+        if(!artist ){
+            res.status(404).json({message:`artist ${id} does not exist`})
+        }
+
+        res.status(200).json(artist)
+        console.log(artist)
+
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+}
+
+
+module.exports = { createArtist, readArtist, getArtistByID }
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const artistController = require('../controllers/artist');
-const { createArtist, readArtist } = require('../controllers/artist');
+const { createArtist, readArtist, getArtistByID } = require('../controllers/artist');
 
 const router = express.Router();
 
 router.post('/', artistController.createArtist);
 
-router.get('/', artistController.readArtist)
+router.get('/', artistController.readArtist);
+
+router.get('/:id', artistController.getArtistByID);
 
 module.exports = router;
